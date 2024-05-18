aab328e3b07bd08d37113a6e3c0e9b6d07f3c4e3
21200164@ue.edu.pe <21200164@ue.edu.pe>
2024-05-18 16:02:20 -0500

    Agregar archivos de proyecto.

---
+++ UE.Parcial20240121200164.API/Data/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UE.Parcial20240121200164.API.Data;
+
+public partial class Movie
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Genre { get; set; }
+
+    public string? ReleaseYear { get; set; }
+}
